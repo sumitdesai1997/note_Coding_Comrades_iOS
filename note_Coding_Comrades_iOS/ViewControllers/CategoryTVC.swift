d4f5d388c3481499c98af251bcb6ffbef651b822
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-21 01:15:08 -0700

    Added CategoryTVC

---
+++ note_Coding_Comrades_iOS/ViewControllers/CategoryTVC.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryTVC.swift
+//  note_Coding_Comrades_iOS
+//
+//  Created by Sumit Desai on 21/05/21.
+//
+
+import UIKit
+
+class CategoryTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
