c80f7c6761252c0ae8b08978dd1b0c1672715be0
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-21 04:59:04 -0700

    1. Adding Category entity list and creating context 2. Adding IBAction for the create category button

--- note_Coding_Comrades_iOS/ViewControllers/CategoryTVC.swift
+++ note_Coding_Comrades_iOS/ViewControllers/CategoryTVC.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class CategoryTVC: UITableViewController {
 
+    // creating a category list array to store the categories
+    var categoryList = [Category]()
+    
+    // creating context object to work with core data
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // reloading the table data just before screen appear
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -17,12 +28,37 @@ class CategoryTVC: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categoryList.count
+    }
+    
+    // creating method for new category addition
+    @IBAction func createCategory(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Create new category", message: "please give a name", preferredStyle: .alert)
+        let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            let categoryNames = self.categoryList.map {$0.name?.lowercased()}
+            guard !categoryNames.contains(textField.text?.lowercased()) else {return}
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categoryList.append(newCategory)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        // change the color of the cancel button action
+        cancelAction.setValue(UIColor.red, forKey: "titleTextColor")
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "folder name"
+        }
+        
+        present(alert, animated: true, completion: nil)
     }
 
 }
