2407bd0be52d147b42e6bf009f009504e7d75b31
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-27 04:03:00 -0700

    implementing the loadRecrdingUI method for recording, and adding start recording and finish recording methods

--- note_Coding_Comrades_iOS/ViewControllers/NoteEditVC.swift
+++ note_Coding_Comrades_iOS/ViewControllers/NoteEditVC.swift
@@ -37,6 +37,24 @@ class NoteEditVC: UIViewController, UIImagePickerControllerDelegate & UINavigati
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // define location manager accuracy
         locationManager.requestWhenInUseAuthorization() // define request authorization
         locationManager.startUpdatingLocation() // start updating the location
+        
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
     }
     
     //*************** IMAGE HANDLING ******************************
@@ -77,6 +95,61 @@ class NoteEditVC: UIViewController, UIImagePickerControllerDelegate & UINavigati
         notePictureImg.image = image
         // print out the image size as a test
         print(image.size)
+    }
+    
+    //*************** AUDIO HANDLING ******************************
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        view.addSubview(recordButton)
+    }
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+    
+    @objc func recordTapped() {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
     }
     
     
