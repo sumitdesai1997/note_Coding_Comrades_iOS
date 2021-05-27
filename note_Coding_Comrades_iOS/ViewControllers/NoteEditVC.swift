d9d7cdc8cb48477c392b87b11840edf9dc386348
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-27 00:44:11 -0700

    Adding NoteEditVC

---
+++ note_Coding_Comrades_iOS/ViewControllers/NoteEditVC.swift
@@ -0,0 +1,30 @@
+//
+//  NoteEditVC.swift
+//  note_Coding_Comrades_iOS
+//
+//  Created by Sumit Desai on 27/05/21.
+//
+
+import UIKit
+import MapKit
+
+class NoteEditVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
